20e1e8eac942718bbd97b5ba74b11c3108e25686
Tamene <tamenes21@gmail.com>
2023-09-06 07:57:27 -0700

    modified

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-  constructore() {
+  constructor() {
     this.host = process.env.DB_HOST || 'localhost';
     this.port = process.env.DB_PORT || '27017';
     this.database = process.env.DB_DATABASE || 'files_manager';
