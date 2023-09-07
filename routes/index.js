28909e0912e4cc2ad12284673a4a5fbb4c01ad1a
Tamene <tamenes21@gmail.com>
2023-09-06 23:29:23 -0700

    connect to database

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,23 @@
 import { Router } from 'express';
-
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
-import UtilController from '../controllers/UtilController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
 const router = Router();
 
-router.use((request, response, next) => {
-  const paths = ['/connect'];
-  if (!paths.includes(request.path)) {
-    next();
-  } else if (!request.headers.authorization) {
-    response.status(401).json({ error: 'Unauthorized' }).end();
-  } else {
-    next();
-  }
-});
-
-router.use((request, response, next) => {
-  const paths = ['/disconnect', '/users/me', '/files'];
-  if (!paths.includes(request.path)) {
-    next();
-  } else if (!request.headers['x-token']) {
-    response.status(401).json({ error: 'Unauthorized' }).end();
-  } else {
-    next();
-  }
-});
-
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.get('/users/me' UsersController.getMe);
 router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+router.get('/connect', basicAuthenticate, AuthController.getConnect);
+router.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+router.post('/files', xTokenAuthenticate, FilesController.postUpload);
+router.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+router.get('/files', xTokenAuthenticate, FilesController.getIndex);
+router.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+router.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
-router.put('/files/:id/publish', UtilController.token, FilesController.putPublish);
-router.put('/files/:id/unpublish', UtilController.token, FilesController.putUnpublish);
-module.exports = router;
+export default router;
